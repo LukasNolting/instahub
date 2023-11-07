699793ab99129412575b408fb9bd1c25796948b3
Lukas Nolting <lukas.nolting@icloud.com>
2023-11-07 21:18:42 +0100

    json

--- script/script.js
+++ script/script.js
@@ -1,3 +1,64 @@
+let posts = [
+  {
+    name: "Angular",
+    time: "1 Std.",
+    location: "Kalifornien",
+    likes: 1582,
+    commentCount: 25,
+    headline: "Die Angular Developer sind jetzt bei InstaHub!",
+    comments: [
+      "Angular ist ein TypeScript-basiertes Webframework.",
+      "Angular ist ein Frontend-Framework für die Entwicklung von Webanwendungen.",
+      "Angular ist ein leistungsstarkes Webentwicklungsframework.",
+    ],
+    image: "./img/profiles/Angular.svg",
+  },
+  {
+    name: "GIT",
+    time: "3 Std.",
+    location: "Brooklyn",
+    likes: 1878,
+    commentCount: 85,
+    headline: "Heute schon gepusht?",
+    comments: [
+      "Git ist ein Versionskontrollsystem für Entwickler.",
+      "Git ist ein Entwicklungs-Tool zur Versionskontrolle.",
+      "Git ist ein Versionsverwaltungssystem.",
+    ],
+    image: "./img/profiles/GIT.svg",
+  },
+  {
+    name: "JavaScript",
+    time: "8 Std.",
+    location: "Kalifornien",
+    likes: 954,
+    commentCount: 12,
+    headline:
+      "Warum mag JavaScript keine Beziehungen? Weil es immer 'null' bedeutet!",
+    comments: [
+      "JavaScript ist eine Web-Programmiersprache.",
+      "JavaScript ist eine Browser-Programmiersprache.",
+      "JavaScript ist eine gängige Web-Scripting-Sprache.",
+    ],
+    image: "./img/profiles/JavaScript.svg",
+  },
+  {
+    name: "HTML",
+    time: "2 Std.",
+    location: "Cambridge",
+    likes: 1023,
+    commentCount: 14,
+    headline:
+      "Warum ist HTML so unaufgeregt? Weil es nur 'Tag's über Tags spricht!",
+    comments: [
+      "HTML ist die Sprache des Webs.",
+      "HTML ist die Basis für Webseiten.",
+      "HTML strukturiert das Web.",
+    ],
+    image: "./img/profiles/HTML.svg",
+  },
+];
+
 function noFunction() {
-    alert("Aktuell keine Funktion! Danke für Dein Verständnis! :)")
+  alert("Aktuell keine Funktion! Danke für dein Verständnis! :)");
 }
